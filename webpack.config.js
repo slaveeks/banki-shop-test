b12c502b4058a64eda0c9e0a2a75a1b7574976d6
slaveeks <chernyshev2001@icloud.com>
2022-10-13 13:41:01 +0300

    Initialized Vue app

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require("vue-loader");
+
+// module
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: { directory: path.join(__dirname, "dist") },
+        port: 9000,
+        open: true,
+    },
+    module: {
+        rules: [ {
+            test:/\.scss$/,
+            use: [{
+                loader: 'style-loader'
+            },
+                {
+                    loader: 'css-loader'
+                },
+                {
+                    loader: 'sass-loader'
+                }
+            ]
+        },
+            {
+                test: /\.vue$/i,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "vue-loader",
+                },
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                }
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            }
+        ]
+    },
+    plugins: [new VueLoaderPlugin(),
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template:"./src/index.html"
+        })]
+};
