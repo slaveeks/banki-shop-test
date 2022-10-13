915a070b7b02dea0f34231603c43797eaa458545
slaveeks <chernyshev2001@icloud.com>
2022-10-13 19:59:56 +0300

    Added input component, components removed to separate directories

---
+++ src/components/Input/Input.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper">
+    <input class="input" placeholder="Поиск по названию картины"/>
+    <Button text="Найти"></Button>
+  </div>
+</template>
+
+<script>
+import Button from "components/Button/Button";
+export default {
+  name: "Input",
+  components: {Button}
+}
+</script>
+
+<style scoped>
+  @import "./Input.styles.scss";
+</style>
